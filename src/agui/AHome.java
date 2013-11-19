5563dc89605fc271c7881c22b4d8c98467d83f2c
Cj Buresch <cjburesch@gmail.com>
2013-11-18 21:14:41 -0800

    Making changes
    And going home to make food.

--- src/agui/AHome.java
+++ src/agui/AHome.java
@@ -3,6 +3,10 @@ package agui;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.TabItem;
 
 public class AHome {
 
@@ -41,9 +45,44 @@ public class AHome {
 	 */
 	protected void createContents() {
 		shlPortunesAdministrator = new Shell();
-		shlPortunesAdministrator.setSize(450, 300);
+		shlPortunesAdministrator.setSize(568, 410);
 		shlPortunesAdministrator.setText("Portunes | Administrator Home");
+		
+		TabFolder _home_tabs = new TabFolder(shlPortunesAdministrator, SWT.NONE);
+		_home_tabs.setBounds(0, 0, 552, 351);
+		
+		TabItem tbtmSearch = new TabItem(_home_tabs, SWT.NONE);
+		tbtmSearch.setText("Search");
+		
+		TabItem tbtmAddNew = new TabItem(_home_tabs, SWT.NONE);
+		tbtmAddNew.setText("Add New User");
+		
+		Menu _home_menu = new Menu(shlPortunesAdministrator, SWT.BAR);
+		shlPortunesAdministrator.setMenuBar(_home_menu);
+		
+		MenuItem mntmFile = new MenuItem(_home_menu, SWT.CASCADE);
+		mntmFile.setText("File");
+		
+		Menu menu = new Menu(mntmFile);
+		mntmFile.setMenu(menu);
+		
+		MenuItem mntmChangeUser = new MenuItem(menu, SWT.NONE);
+		mntmChangeUser.setText("Logout");
+		
+		MenuItem mntmExit = new MenuItem(menu, SWT.NONE);
+		mntmExit.setText("Exit");
+		
+		MenuItem mntmEdit = new MenuItem(_home_menu, SWT.CASCADE);
+		mntmEdit.setText("Edit");
+		
+		Menu menu_1 = new Menu(mntmEdit);
+		mntmEdit.setMenu(menu_1);
+		
+		MenuItem mntmAbout = new MenuItem(_home_menu, SWT.NONE);
+		mntmAbout.setText("About");
+		
+		MenuItem mntmHelp = new MenuItem(_home_menu, SWT.NONE);
+		mntmHelp.setText("Help");
 
 	}
-
 }
