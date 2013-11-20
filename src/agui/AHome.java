05dd831b82d0068f3693ea6b18f3dc54b32f1ef5
Cj Buresch <cjburesch@gmail.com>
2013-11-20 14:09:21 -0800

    Minor UI Changes pt II
    Return of the UI

--- src/agui/AHome.java
+++ src/agui/AHome.java
@@ -18,6 +18,10 @@ public class AHome {
 	protected Shell shlPortunesAdministrator;
 	private Text text;
 	private Text text_1;
+	private Text text_2;
+	private Text text_3;
+	private Text text_4;
+	private Text text_5;
 
 	/**
 	 * Launch the application.
@@ -68,7 +72,7 @@ public class AHome {
 		grpSearchForSpecifc.setBounds(10, 27, 355, 96);
 		
 		Label lblNewLabel = new Label(grpSearchForSpecifc, SWT.NONE);
-		lblNewLabel.setBounds(10, 10, 426, 15);
+		lblNewLabel.setBounds(10, 10, 335, 15);
 		lblNewLabel.setText("You are able to search by either a username or the user's name. ");
 		
 		Label lblUsername = new Label(grpSearchForSpecifc, SWT.NONE);
@@ -89,7 +93,7 @@ public class AHome {
 		grpSearchByAdvanced.setBounds(10, 155, 355, 62);
 		
 		Button btnNewButton = new Button(grpSearchByAdvanced, SWT.NONE);
-		btnNewButton.setBounds(109, 27, 126, 25);
+		btnNewButton.setBounds(108, 27, 126, 25);
 		btnNewButton.setText("Show query list");
 		
 		Button btnSearchForUser = new Button(composite, SWT.RADIO);
@@ -101,11 +105,58 @@ public class AHome {
 		btnRadioButton.setText("Seach database using advanced queries");
 		
 		Button btnNewButton_1 = new Button(composite, SWT.NONE);
-		btnNewButton_1.setBounds(235, 261, 130, 52);
+		btnNewButton_1.setBounds(235, 274, 130, 39);
 		btnNewButton_1.setText("Show Results");
 		
 		TabItem tbtmAddNew = new TabItem(_home_tabs, SWT.NONE);
 		tbtmAddNew.setText("Add New User");
+		
+		Composite composite_1 = new Composite(_home_tabs, SWT.NONE);
+		tbtmAddNew.setControl(composite_1);
+		
+		Group grpNewUserInformation = new Group(composite_1, SWT.NONE);
+		grpNewUserInformation.setText("New User Information");
+		grpNewUserInformation.setBounds(10, 46, 354, 153);
+		
+		Label lblUsername_1 = new Label(grpNewUserInformation, SWT.NONE);
+		lblUsername_1.setBounds(39, 33, 63, 15);
+		lblUsername_1.setText("Username:");
+		
+		Label lblName = new Label(grpNewUserInformation, SWT.NONE);
+		lblName.setBounds(39, 55, 55, 15);
+		lblName.setText("Name:");
+		
+		Label lblPassword = new Label(grpNewUserInformation, SWT.NONE);
+		lblPassword.setBounds(39, 90, 63, 15);
+		lblPassword.setText("Password:");
+		
+		Label lblConfirmPassword = new Label(grpNewUserInformation, SWT.NONE);
+		lblConfirmPassword.setBounds(39, 115, 104, 15);
+		lblConfirmPassword.setText("Confirm Password:");
+		
+		text_2 = new Text(grpNewUserInformation, SWT.BORDER);
+		text_2.setBounds(97, 27, 210, 21);
+		
+		text_3 = new Text(grpNewUserInformation, SWT.BORDER);
+		text_3.setBounds(79, 54, 228, 21);
+		
+		text_4 = new Text(grpNewUserInformation, SWT.BORDER);
+		text_4.setBounds(97, 87, 210, 21);
+		
+		text_5 = new Text(grpNewUserInformation, SWT.BORDER);
+		text_5.setBounds(141, 112, 166, 21);
+		
+		Label lblErrorLabelRight = new Label(composite_1, SWT.NONE);
+		lblErrorLabelRight.setBounds(25, 21, 244, 15);
+		lblErrorLabelRight.setText("Error Label right here ");
+		
+		Button btnNewButton_2 = new Button(composite_1, SWT.NONE);
+		btnNewButton_2.setBounds(247, 261, 117, 52);
+		btnNewButton_2.setText("Add User");
+		
+		Button btnNewButton_3 = new Button(composite_1, SWT.NONE);
+		btnNewButton_3.setBounds(10, 261, 117, 52);
+		btnNewButton_3.setText("Clear");
 		
 		Menu _home_menu = new Menu(shlPortunesAdministrator, SWT.BAR);
 		shlPortunesAdministrator.setMenuBar(_home_menu);
