0c47471a10b3076d59f46de227fc3e654f4bbd34
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2013-12-07 11:28:16 -0800

    ehlp me?

--- src/agui/Alogin.java
+++ src/agui/Alogin.java
@@ -1,5 +1,9 @@
 package agui;
 
+import java.awt.event.ActionListener;
+
+import auth.*;
+
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Composite;
@@ -10,14 +14,18 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 
 public class Alogin {
-
+	
 	protected Shell Alogin;
 	private Text text;
 	private Text text_1;
-
+	private AuthClient portclient;
 	/**
 	 * Open the window.
 	 */
+	public Alogin(AuthClient tempclient){
+		portclient = tempclient;
+	}
+	
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -36,32 +44,57 @@ public class Alogin {
 	 */
 	protected void createContents() {
 		Alogin = new Shell();
-		Alogin.setSize(354, 183);
+		Alogin.setSize(376, 204);
 		Alogin.setText("Portunes | Administer Login");
 		
-		Composite composite = new Composite(Alogin, SWT.NONE);
-		composite.setBounds(0, 0, 348, 155);
-		
 		Group grpEnterAdminInformation = new Group(Alogin, SWT.NONE);
-		grpEnterAdminInformation.setBounds(0, 0, 348, 155);
+		grpEnterAdminInformation.setBounds(10, 10, 338, 144);
 		
 		Label lblAdministrator = new Label(grpEnterAdminInformation, SWT.NONE);
-		lblAdministrator.setBounds(22, 36, 73, 15);
+		lblAdministrator.setBounds(10, 27, 73, 15);
 		lblAdministrator.setText("Administrator:");
 		
 		Label lblPassword = new Label(grpEnterAdminInformation, SWT.NONE);
-		lblPassword.setBounds(22, 67, 55, 15);
+		lblPassword.setBounds(10, 58, 55, 15);
 		lblPassword.setText("Password:");
 		
 		text = new Text(grpEnterAdminInformation, SWT.BORDER);
-		text.setBounds(101, 33, 223, 21);
+		text.setBounds(89, 24, 235, 21);
 		
 		text_1 = new Text(grpEnterAdminInformation, SWT.BORDER);
-		text_1.setBounds(83, 67, 241, 22);
+		text_1.setBounds(71, 58, 253, 22);
 		
 		Button _login_button = new Button(grpEnterAdminInformation, SWT.NONE);
-		_login_button.setBounds(215, 106, 123, 37);
+		_login_button.setBounds(201, 95, 123, 37);
 		_login_button.setText("Login");
+		_login_button.addActionListener(new ButtonListener());
 
 	}
+	private class ButtonListener implements ActionListener
+	  {
+	       public void actionPerformed(ActionEvent event)
+	        {
+	            Object bpress = event.getSource();
+
+	        if(bpress == addPet){
+	            for (int p = 1; p < selectList.size(); p++){
+	                boolean check = false;
+	                String selectedPet =(String) petList.getSelectedValue();
+	                String petCheck =(String) selectList.get(p);
+	                if(selectedPet.equals(petCheck)){
+	                    check = true;
+	                    break;
+	                } else if(added == false){
+	                    petsAvail.add(selectedPet);
+	                    petsAvail.updateUI();
+	                    numPets++;
+	                }
+	            }
+	        } else if (bpress == remove){
+
+	            numPets--;
+	        }
+	        }
+	  }
+	
 }
