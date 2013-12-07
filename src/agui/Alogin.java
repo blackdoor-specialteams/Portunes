4bac207c17e60b4df8a11039bd5142f372f6e8d0
Cj Buresch <cjburesch@gmail.com>
2013-12-07 11:46:37 -0800

    Work

--- src/agui/Alogin.java
+++ src/agui/Alogin.java
@@ -1,5 +1,6 @@
 package agui;
 
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import auth.*;
@@ -14,18 +15,19 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.Button;
 
 public class Alogin {
-	
+
 	protected Shell Alogin;
 	private Text text;
 	private Text text_1;
 	private AuthClient portclient;
+
 	/**
 	 * Open the window.
 	 */
-	public Alogin(AuthClient tempclient){
+	public Alogin(AuthClient tempclient) {
 		portclient = tempclient;
 	}
-	
+
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
@@ -40,61 +42,45 @@ public class Alogin {
 
 	/**
 	 * Create contents of the window.
+	 * 
 	 * @wbp.parser.entryPoint
 	 */
 	protected void createContents() {
 		Alogin = new Shell();
 		Alogin.setSize(376, 204);
 		Alogin.setText("Portunes | Administer Login");
-		
+
 		Group grpEnterAdminInformation = new Group(Alogin, SWT.NONE);
 		grpEnterAdminInformation.setBounds(10, 10, 338, 144);
-		
+
 		Label lblAdministrator = new Label(grpEnterAdminInformation, SWT.NONE);
 		lblAdministrator.setBounds(10, 27, 73, 15);
 		lblAdministrator.setText("Administrator:");
-		
+
 		Label lblPassword = new Label(grpEnterAdminInformation, SWT.NONE);
 		lblPassword.setBounds(10, 58, 55, 15);
 		lblPassword.setText("Password:");
-		
+
 		text = new Text(grpEnterAdminInformation, SWT.BORDER);
 		text.setBounds(89, 24, 235, 21);
-		
+
 		text_1 = new Text(grpEnterAdminInformation, SWT.BORDER);
 		text_1.setBounds(71, 58, 253, 22);
-		
+
 		Button _login_button = new Button(grpEnterAdminInformation, SWT.NONE);
+		_login_button.addActionListener(new ButtonListener());
 		_login_button.setBounds(201, 95, 123, 37);
 		_login_button.setText("Login");
-		_login_button.addActionListener(new ButtonListener());
 
 	}
-	private class ButtonListener implements ActionListener
-	  {
-	       public void actionPerformed(ActionEvent event)
-	        {
-	            Object bpress = event.getSource();
 
-	        if(bpress == addPet){
-	            for (int p = 1; p < selectList.size(); p++){
-	                boolean check = false;
-	                String selectedPet =(String) petList.getSelectedValue();
-	                String petCheck =(String) selectList.get(p);
-	                if(selectedPet.equals(petCheck)){
-	                    check = true;
-	                    break;
-	                } else if(added == false){
-	                    petsAvail.add(selectedPet);
-	                    petsAvail.updateUI();
-	                    numPets++;
-	                }
-	            }
-	        } else if (bpress == remove){
+	public class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Event 
 
-	            numPets--;
-	        }
-	        }
-	  }
-	
+		}
+
+	}
+
 }
--- src/agui/RunAdminGUI.java
+++ src/agui/RunAdminGUI.java
@@ -3,10 +3,13 @@ package agui;
 import auth.*;
 
 public class RunAdminGUI {
-
+	final private String servername = "localhost";
+	final private int serverport = 30; 
+	private static AuthClient myclient;
 	public static void main(String[] args) {
+		myclient = new AuthClient(servername,serverport);
 		try {
-			Alogin login = new Alogin();
+			Alogin login = new Alogin(myclient);
 			login.open();
 		} catch (Exception e) {
 			e.printStackTrace();
