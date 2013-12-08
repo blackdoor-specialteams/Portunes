e7a44f01425c505a49d34241c22e14cd2531fc99
Cj Buresch <cjburesch@gmail.com>
2013-12-08 13:19:01 -0800

    More Work
    +login should work when the resolver does
    +sanitized inputs from login
    +AHome is getting there.
    Next stop: handling queries.

--- src/agui/RunAdminGUI.java
+++ src/agui/RunAdminGUI.java
@@ -3,20 +3,19 @@ package agui;
 import auth.*;
 
 public class RunAdminGUI {
-	final static private String servername = "localhost";
-	final static private int serverport = 30; 
+	final static private String servername = "vodkapi.dyndns";
+	final static private int serverport = 3306;
 	public static AuthClient myclient;
-	
+
 	public static void main(String[] args) {
-		
-		myclient = new AuthClient(servername,serverport);
-		try {
-			Alogin login = new Alogin(myclient);
-			login.open();
-		} catch (Exception e) {
-			e.printStackTrace();
+
+		myclient = new AuthClient(servername, serverport);
+
+		Alogin login = new Alogin(myclient);
+
+		if (login.open()) {
+			AHome home = new AHome(myclient);
+			home.open();
 		}
-
 	}
-
 }
