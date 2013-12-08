08faf657745508969c55a4aced7e5737a00435c6
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2013-12-08 15:20:28 -0800

    /

--- src/auth/ArbitraryRequest.java
+++ src/auth/ArbitraryRequest.java
@@ -6,10 +6,12 @@ import java.util.Map;
 
 public class ArbitraryRequest extends Request implements Serializable {
 	List<Map<String, Object>> reply;
+	String query;
 	public ArbitraryRequest(String query, String adminName, byte[] adminPW) {
 		super(null);
 		this.adminName = adminName;
 		this.adminPW = adminPW;		
+		this.query = query;
 	}
 	
 		public String getAuthUserName(){
